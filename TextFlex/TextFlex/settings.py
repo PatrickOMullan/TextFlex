c44c65ce173b139abb338c5fb42b43684448dd1f
PatrickOMullan <36465271+PatrickOMullan@users.noreply.github.com>
2020-12-09 21:51:44 -0500

    removed personal info

--- TextFlex/TextFlex/settings.py
+++ TextFlex/TextFlex/settings.py
@@ -135,5 +135,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'textflexwpi@gmail.com'
-EMAIL_HOST_PASSWORD = 'Oct16ber!!!'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
